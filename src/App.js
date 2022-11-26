07d7d4e5d014d20e65c5b123a1b1718c2a000519
yazimel.niclis <yazimelniclis1995@gmail.com>
2022-11-26 12:42:45 -0300

    Integracion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Form from "./components/form/form";
-
+import { consultarBackend } from "./api/Rule_telefonos";
 function App() {
+  const [mensaje, setMensaje] = useState("");
+
+  const consultarBack = async () => {
+    await consultarBackend()
+      .then((resultado) => {
+        setMensaje(resultado);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  useEffect(() => {
+    consultarBack();
+  }, []);
   return (
-    <div className="App">
-      <Form />
-    </div>
+    <div className="App">{mensaje && <p>{mensaje.time.toString()}</p>}</div>
   );
 }
 
--- src/api/Rule_telefonos.jsx
+++ src/api/Rule_telefonos.jsx
@@ -1,5 +1,17 @@
 import API from "./Rule_API";
 
+export const consultarBackend = async () => {
+  let url = "/api";
+  return await API.get(url)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error.response.data.error || "Error procesando la solicitud";
+    });
+};
+
 export const obtenerTelefonos = async () => {
   let url = "/api/telefonos";
   return await API.get(url)
